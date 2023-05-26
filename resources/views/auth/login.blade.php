12e3e0dce843c9970692b4de1b9fca7921378619
= <=>
2023-05-26 09:02:52 +0700

    coba

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,10 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
+    
     <form method="POST" action="{{ route('login') }}">
         @csrf
-
+       
         <!-- Alamat Email -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
@@ -32,7 +32,7 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="forgot">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
@@ -45,3 +45,4 @@
         </div>
     </form>
 </x-guest-layout>
+
