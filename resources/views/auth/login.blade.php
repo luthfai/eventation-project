5b7c047a2c8af24c78392e3e343ee8a7219592ee
= <=>
2023-05-26 09:39:15 +0700

    membuat login.css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,12 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-    
+  
     <form method="POST" action="{{ route('login') }}">
         @csrf
        
+        <link href="css/login.css" rel="stylesheet">
+
         <!-- Alamat Email -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
