ab4b4b5f591079e4566ebcb659afb97c178229d8
= <=>
2023-06-10 21:51:32 +0700

    detail undangan

--- resources/views/detail-undangan.blade.php
+++ resources/views/detail-undangan.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
     <x-header></x-header>
-    
-    @dd($undangan)
-<!--
+  <!--  
+
+
   This example requires some changes to your config:
   
   ```
@@ -32,7 +32,7 @@
       <ol role="list" class="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <li>
           <div class="flex items-center">
-            <a href="#" class="mr-2 text-sm font-medium text-gray-900">Men</a>
+            <a href="#" class="mr-2 text-sm font-medium text-gray-900">All Preset</a>
             <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" aria-hidden="true" class="h-5 w-4 text-gray-300">
               <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
             </svg>
@@ -40,7 +40,7 @@
         </li>
         <li>
           <div class="flex items-center">
-            <a href="#" class="mr-2 text-sm font-medium text-gray-900">Clothing</a>
+            <a href="#" class="mr-2 text-sm font-medium text-gray-900">Desain</a>
             <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" aria-hidden="true" class="h-5 w-4 text-gray-300">
               <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
             </svg>
@@ -48,7 +48,7 @@
         </li>
 
         <li class="text-sm">
-          <a href="#" aria-current="page" class="font-medium text-gray-500 hover:text-gray-600">Basic Tee 6-Pack</a>
+          <a href="#" aria-current="page" class="font-medium text-gray-500 hover:text-blue-600">Golden Floral</a>
         </li>
       </ol>
     </nav>
@@ -56,31 +56,31 @@
     <!-- Image gallery -->
     <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
       <div class="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
-        <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg" alt="Two each of gray, white, and black shirts laying flat." class="h-full w-full object-cover object-center">
+        <img src="https://img.freepik.com/free-vector/engraving-hand-drawn-golden-wedding-invitation-template_79603-1845.jpg?w=740&t=st=1686403654~exp=1686404254~hmac=18b74e5db1764500468f1bceed0bbd43a95501c832830709d4bb240643fe01b6">
       </div>
       <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
         <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg" alt="Model wearing plain black basic tee." class="h-full w-full object-cover object-center">
+          <img src="https://img.freepik.com/premium-vector/hand-drawn-engraved-wedding-logo-collection_23-2149352770.jpg?w=1480" alt="Model wearing plain black basic tee." class="h-full w-full object-cover object-center">
         </div>
         <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg" alt="Model wearing plain gray basic tee." class="h-full w-full object-cover object-center">
+          <img src="https://img.freepik.com/premium-vector/golden-floral-frame-template-collection_67638-11.jpg?w=1380" alt="Model wearing plain gray basic tee." class="h-full w-full object-cover object-center">
         </div>
       </div>
       <div class="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
-        <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg" alt="Model wearing plain white basic tee." class="h-full w-full object-cover object-center">
+        <img src="https://img.freepik.com/free-vector/gradient-golden-luxury-wedding-invitation-with-photo_52683-69309.jpg?w=740&t=st=1686402054~exp=1686402654~hmac=3be4dd6e0f09e9f2f24c698768482068af29cee103e62323c08dc06ed3a60221" alt="Two each of gray, white, and black shirts laying flat." class="h-full w-full object-cover object-center" alt="Model wearing plain white basic tee." class="h-full w-full object-cover object-center">
       </div>
     </div>
 
     <!-- Product info -->
     <div class="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
       <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-        <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Basic Tee 6-Pack</h1>
+        <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Golden Floral</h1>
       </div>
 
       <!-- Options -->
       <div class="mt-4 lg:row-span-3 lg:mt-0">
         <h2 class="sr-only">Product information</h2>
-        <p class="text-3xl tracking-tight text-gray-900">$192</p>
+        <p class="text-3xl tracking-tight text-gray-900">Rp40.000,00</p>
 
         <!-- Reviews -->
         <div class="mt-6">
@@ -105,7 +105,7 @@
               </svg>
             </div>
             <p class="sr-only">4 out of 5 stars</p>
-            <a href="#" class="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">117 reviews</a>
+            <a href="#" class="ml-3 text-sm font-medium text-red-600 hover:text-gray-500">117 reviews</a>
           </div>
         </div>
 
@@ -131,9 +131,9 @@
                   Not Active and Checked: "ring-2"
                 -->
                 <label class="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-gray-400">
-                  <input type="radio" name="color-choice" value="Gray" class="sr-only" aria-labelledby="color-choice-1-label">
-                  <span id="color-choice-1-label" class="sr-only">Gray</span>
-                  <span aria-hidden="true" class="h-8 w-8 bg-gray-200 rounded-full border border-black border-opacity-10"></span>
+                  <input type="radio" name="color-choice" value="gold" class="sr-only" aria-labelledby="color-choice-1-label">
+                  <span id="color-choice-1-label" class="sr-only">gold</span>
+                  <span aria-hidden="true" class="h-8 w-8 bg-yellow-700 rounded-full border border-black border-opacity-10"></span>
                 </label>
                 <!--
                   Active and Checked: "ring ring-offset-1"
@@ -151,27 +151,27 @@
           <!-- Sizes -->
           <div class="mt-10">
             <div class="flex items-center justify-between">
-              <h3 class="text-sm font-medium text-gray-900">Size</h3>
-              <a href="#" class="text-sm font-medium text-indigo-600 hover:text-indigo-500">Size guide</a>
+              <h3 class="text-sm font-medium text-gray-900">What is the count of attendees?</h3>
+              <a href="#" class="text-sm font-medium text-red-600 hover:text-gray-500">Select</a>
             </div>
 
             <fieldset class="mt-4">
               <legend class="sr-only">Choose a size</legend>
               <div class="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
                 <!-- Active: "ring-2 ring-indigo-500" -->
-                <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-not-allowed bg-gray-50 text-gray-200">
-                  <input type="radio" name="size-choice" value="XXS" disabled class="sr-only" aria-labelledby="size-choice-0-label">
-                  <span id="size-choice-0-label">XXS</span>
-                  <span aria-hidden="true" class="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200">
-                    <svg class="absolute inset-0 h-full w-full stroke-2 text-gray-200" viewBox="0 0 100 100" preserveAspectRatio="none" stroke="currentColor">
-                      <line x1="0" y1="100" x2="100" y2="0" vector-effect="non-scaling-stroke" />
-                    </svg>
-                  </span>
+                <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
+                  <input type="radio" name="size-choice" value="XS" class="sr-only" aria-labelledby="size-choice-1-label">
+                  <span id="size-choice-1-label">50</span>
+                  <!--
+                    Active: "border", Not Active: "border-2"
+                    Checked: "border-indigo-500", Not Checked: "border-transparent"
+                  -->
+                  <span class="pointer-events-none absolute -inset-px rounded-md" aria-hidden="true"></span>
                 </label>
                 <!-- Active: "ring-2 ring-indigo-500" -->
                 <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
                   <input type="radio" name="size-choice" value="XS" class="sr-only" aria-labelledby="size-choice-1-label">
-                  <span id="size-choice-1-label">XS</span>
+                  <span id="size-choice-1-label">100</span>
                   <!--
                     Active: "border", Not Active: "border-2"
                     Checked: "border-indigo-500", Not Checked: "border-transparent"
@@ -181,7 +181,7 @@
                 <!-- Active: "ring-2 ring-indigo-500" -->
                 <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
                   <input type="radio" name="size-choice" value="S" class="sr-only" aria-labelledby="size-choice-2-label">
-                  <span id="size-choice-2-label">S</span>
+                  <span id="size-choice-2-label">150</span>
                   <!--
                     Active: "border", Not Active: "border-2"
                     Checked: "border-indigo-500", Not Checked: "border-transparent"
@@ -191,7 +191,7 @@
                 <!-- Active: "ring-2 ring-indigo-500" -->
                 <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
                   <input type="radio" name="size-choice" value="M" class="sr-only" aria-labelledby="size-choice-3-label">
-                  <span id="size-choice-3-label">M</span>
+                  <span id="size-choice-3-label">200</span>
                   <!--
                     Active: "border", Not Active: "border-2"
                     Checked: "border-indigo-500", Not Checked: "border-transparent"
@@ -201,7 +201,7 @@
                 <!-- Active: "ring-2 ring-indigo-500" -->
                 <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
                   <input type="radio" name="size-choice" value="L" class="sr-only" aria-labelledby="size-choice-4-label">
-                  <span id="size-choice-4-label">L</span>
+                  <span id="size-choice-4-label">250</span>
                   <!--
                     Active: "border", Not Active: "border-2"
                     Checked: "border-indigo-500", Not Checked: "border-transparent"
@@ -211,7 +211,7 @@
                 <!-- Active: "ring-2 ring-indigo-500" -->
                 <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
                   <input type="radio" name="size-choice" value="XL" class="sr-only" aria-labelledby="size-choice-5-label">
-                  <span id="size-choice-5-label">XL</span>
+                  <span id="size-choice-5-label">300</span>
                   <!--
                     Active: "border", Not Active: "border-2"
                     Checked: "border-indigo-500", Not Checked: "border-transparent"
@@ -221,7 +221,7 @@
                 <!-- Active: "ring-2 ring-indigo-500" -->
                 <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
                   <input type="radio" name="size-choice" value="2XL" class="sr-only" aria-labelledby="size-choice-6-label">
-                  <span id="size-choice-6-label">2XL</span>
+                  <span id="size-choice-6-label">400</span>
                   <!--
                     Active: "border", Not Active: "border-2"
                     Checked: "border-indigo-500", Not Checked: "border-transparent"
@@ -229,9 +229,9 @@
                   <span class="pointer-events-none absolute -inset-px rounded-md" aria-hidden="true"></span>
                 </label>
                 <!-- Active: "ring-2 ring-indigo-500" -->
-                <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
+                <label class="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-4 cursor-pointer bg-white text-gray-900 shadow-sm">
                   <input type="radio" name="size-choice" value="3XL" class="sr-only" aria-labelledby="size-choice-7-label">
-                  <span id="size-choice-7-label">3XL</span>
+                  <span id="size-choice-5-label">400<</span>
                   <!--
                     Active: "border", Not Active: "border-2"
                     Checked: "border-indigo-500", Not Checked: "border-transparent"
@@ -242,17 +242,23 @@
             </fieldset>
           </div>
 
-          <button type="submit" class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Add to bag</button>
+          <button type="submit" class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-red-700 px-8 py-3 text-base font-medium text-white hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Preview Preset</button>
         </form>
       </div>
 
-      <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+      <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r justify-center lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
         <!-- Description and details -->
         <div>
           <h3 class="sr-only">Description</h3>
 
           <div class="space-y-6">
-            <p class="text-base text-gray-900">The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: &quot;Black&quot;. Need to add an extra pop of color to your outfit? Our white tee has you covered.</p>
+            <p class="text-base text-gray-900"> Tema Golden Floral adalah sebuah template undangan yang dipenuhi dengan sentuhan keemasan (golden) dan motif bunga (floral). Template ini menggabungkan elegansi dan keindahan alam untuk menciptakan suasana yang mewah dan romantis.
+
+Desain undangan ini menampilkan latar belakang berwarna emas yang menciptakan kesan mewah dan glamour. Di sekitar undangan, terdapat elemen floral yang indah dengan bunga-bunga yang dirancang dengan detail. Bunga-bunga tersebut memberikan sentuhan kelembutan dan keindahan alami pada template.
+
+Font yang digunakan pada undangan ini adalah font yang elegan dan legible. Huruf-huruf yang tercetak dengan indah memberikan kesan formal dan memperkuat kesan mewah pada undangan.
+
+</p>
           </div>
         </div>
 
@@ -261,19 +267,20 @@
 
           <div class="mt-4">
             <ul role="list" class="list-disc space-y-2 pl-4 text-sm">
-              <li class="text-gray-400"><span class="text-gray-600">Hand cut and sewn locally</span></li>
-              <li class="text-gray-400"><span class="text-gray-600">Dyed with our proprietary colors</span></li>
-              <li class="text-gray-400"><span class="text-gray-600">Pre-washed &amp; pre-shrunk</span></li>
-              <li class="text-gray-400"><span class="text-gray-600">Ultra-soft 100% cotton</span></li>
+              <li class="text-gray-400"><span class="text-gray-600">Elemen Emas yang Mewah</span></li>
+              <li class="text-gray-400"><span class="text-gray-600">Motif Bunga yang Indah</span></li>
+              <li class="text-gray-400"><span class="text-gray-600">Teks Elegan</span></li>
+              <li class="text-gray-400"><span class="text-gray-600">Suasana Mewah dan Romantis</span></li>
+              <li class="text-gray-400"><span class="text-gray-600">Fleksibilitas</span></li>
             </ul>
           </div>
         </div>
 
         <div class="mt-10">
-          <h2 class="text-sm font-medium text-gray-900">Details</h2>
+          <h2 class="text-sm font-medium text-gray-900">Note :</h2>
 
           <div class="mt-4 space-y-6">
-            <p class="text-sm text-gray-600">The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming &quot;Charcoal Gray&quot; limited release.</p>
+            <p class="text-sm text-gray-600">Semua Template Undangan berisikan Nama , Foto, Lokasi Acara, Tempat, Waktu, dll</p>
           </div>
         </div>
       </div>
