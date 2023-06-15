2468e24a63263c7bdaf1a3b4ef4a93544e5ba87e
luthf-ai <aido.el.hakim@gmail.com>
2023-06-15 10:04:21 +0700

    dummy undangan

--- database/seeders/UndanganSeeder.php
+++ database/seeders/UndanganSeeder.php
@@ -38,5 +38,29 @@ class UndanganSeeder extends Seeder
             'paket' => 'Basic',
             'kategori' => 'Wedding'
         ]);
+        DB::table('undangans')->insert([
+            'name' => 'Dummy',
+            'description' => 'Dummy adalah undangan yang dibuat dengan desain elegan dan mewah, cocok untuk kamu yang ingin mengadakan acara pernikahan dengan tema elegan dan mewah, dengan harga yang terjangkau.',
+            'price' => 100000,
+            'slug' => 'undangan2',
+            'image' => 'thumb-u2-1.png',
+            'image2' => 'thumb-u2-2.png',
+            'image3' => 'thumb-u2-3.png',
+            'image4' => 'thumb-u2-4.png',
+            'paket' => 'Basic',
+            'kategori' => 'Wedding'
+        ]);
+        DB::table('undangans')->insert([
+            'name' => 'Dummy2',
+            'description' => 'Dummy adalah undangan yang dibuat dengan desain elegan dan mewah, cocok untuk kamu yang ingin mengadakan acara pernikahan dengan tema elegan dan mewah, dengan harga yang terjangkau.',
+            'price' => 100000,
+            'slug' => 'undangan1',
+            'image' => 'thumb-u1-1.png',
+            'image2' => 'thumb-u1-2.png',
+            'image3' => 'thumb-u1-3.png',
+            'image4' => 'thumb-u1-4.png',
+            'paket' => 'Basic',
+            'kategori' => 'Wedding'
+        ]);
     }
 }
